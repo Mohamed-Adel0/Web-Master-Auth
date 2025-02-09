0e4dda97981bab5f8fe809c0c0b394d9ac1abceb
Mohamed-Adel <128996656+Mohamed-Adel0@users.noreply.github.com>
2025-02-09 20:31:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ createAccount.addEventListener("click", (e) => {
         storageData.push(FetchData);
         localStorage.setItem("users", JSON.stringify(storageData));
         password.classList.remove("Error-input");
-        location.pathname = "/https://mohamed-adel0.github.io/Web-Master-Auth/index.html";
+        location.pathname = "/index.html";
       } else {
         password.classList.add("Error-input");
         document.getElementById("errors").innerText =
